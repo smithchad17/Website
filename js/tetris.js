7617f82504e8c54782fa0a0fd87bb8dac2cd86aa
Chad Smith <smithchad17@gmail.com>
2022-02-13 00:30:52 -0500

    Creating tetris javascript game page

---
+++ js/tetris.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const width = 10
+    const grid = document.querySelector('.grid')
+    let squares = Array.from(document.querySelectorAll('.grid div'))
+    const ScoreDisplay = document.querySelector('#score')
+    const StartBtn = document.querySelector('#start-button')
+    
+})
