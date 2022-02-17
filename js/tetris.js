d67af1675883d464b270f5d1e7549db629cc8c0d
Chad Smith <smithchad17@gmail.com>
2022-02-16 21:50:47 -0500

    javascript update. Css updates - tetris

--- js/tetris.js
+++ js/tetris.js
@@ -4,5 +4,63 @@ document.addEventListener('DOMContentLoaded', () => {
     let squares = Array.from(document.querySelectorAll('.grid div'))
     const ScoreDisplay = document.querySelector('#score')
     const StartBtn = document.querySelector('#start-button')
+
+    const lTetromino = [
+        [1, width+1, width*2+1, 2],
+        [width, width+1, width+2, width*2+2],
+        [1, width+1, width*2+1, width*2],
+        [width, width*2, width*2+1, width*2+2]
+    ]
     
+    const zTetromino = [
+        [width+1, width+1, width+2, width*2+1],
+        [0, width, width+1, width*2],
+        [width*3, width*3+1, width*2, width*2+1],
+        [0, width, width+1, width*2]
+    ]
+
+    const tTetromino = [
+        [2, width, width+1, width+2],
+        [2, width+1, width+2, width*2],
+        [width, width+1, width+2, width*2+1],
+        [1, width, width+1, width*2+1]
+    ]
+
+    const oTetromino = [
+        [0, 1, width, width+1],
+        [0, 1, width, width+1],
+        [0, 1, width, width+1],
+        [0, 1, width, width+1]
+    ]
+
+    const iTetromino = [
+        [1, width+1, width*2+1, width*3+1],
+        [width, width+1, width+2, width+3],
+        [1, width+1, width*2+1, width*3+1],
+        [width, width+1, width+2, width+3]
+    ]
+
+    const theTetrominoes = [lTetromino, zTetromino, tTetromino, 
+        oTetromino, iTetromino]
+
+    let random = Math.floor(Math.random()*theTetrominoes.length)
+    let current = theTetrominoes[random][0];
+    let currentRotation = 0;
+    let currentPosition = 4;
+
+    function draw() {
+        current.forEach(index => {
+            squares[currentPosition + index].classList.add('tetromino')
+        })
+    }
+
+    //undraw the Tetromino
+    function undraw() {
+        current.forEach(index => {
+            squares[currentPosition + index].classList.remove('tetromino')
+        })
+    }
+
+    
+
 })
