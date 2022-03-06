b318527332e7fe0aa796c6a15dd7f5b3d771b032
Chad Smith <smithchad17@gmail.com>
2022-03-06 09:56:56 -0500

    js changes

--- js/tetris.js
+++ js/tetris.js
@@ -67,6 +67,12 @@ document.addEventListener('DOMContentLoaded', () => {
     function control(e) {
         if(e.keyCode === 37) {
             moveLeft()
+        } else if (e.keyCode === 38){
+            //rotate()
+        } else if (e.keyCode === 39){
+            moveRight()
+        } else if (e.keyCode === 40){
+            //moveDown()
         }
     }
 
@@ -103,4 +109,31 @@ document.addEventListener('DOMContentLoaded', () => {
 
         draw()
     }
+
+    function moveRight() {
+        undraw()
+        const isAtRightEdge = current.some(index => (currentPosition + index) % width === width - 1)
+
+        if(!isAtRightEdge) currentPosition += 1
+
+        if(current.some(index => squares[currentPosition + index].classList.contains('taken'))){
+            currentPosition -= 1
+        }
+
+        draw()
+    }
+
+    function moveDown() {
+        undraw()
+        const isAtRightEdge = current.some(index => (currentPosition + index) % width === width - 1)
+
+        if(!isAtRightEdge) currentPosition += 1
+
+        if(current.some(index => squares[currentPosition + index].classList.contains('taken'))){
+            currentPosition -= 1
+        }
+
+        draw()
+    }
+
 })
